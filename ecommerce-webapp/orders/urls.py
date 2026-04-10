1479def1d688a5e2789a1c4287b1df44bbd39ecd
prakashdagade7777-prog <prakashdagade7777@gmail.com>
2026-04-10 09:45:49 +0530

    Delete ecommerce-webapp directory

--- ecommerce-webapp/orders/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.OrderListView.as_view(), name='order-list'),
-    path('<int:pk>/', views.OrderDetailView.as_view(), name='order-detail'),
-]
